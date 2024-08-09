e5e164b0e93211b3399a27c874bc33635f58ef1d
Ouran <cristhiandev20@gmail.com>
2024-08-09 12:54:56 -0300

    Jantar Dos Vampiros
    Trabalho desenvolvido para a matéria de programação concorrente. O jantar dos vampiros, originalmente filósofos, é um problema sobre a sincronização na comunicação entre processos e threads.

---
+++ Jantar_Dos_Vampiros/Filosofos.java
@@ -0,0 +1,357 @@
+/* ***************************************************************
+* Autor............: Cristhian Kauan Moreno Silveira
+* Matricula........: 202210185
+* Inicio...........: 23/10/2023
+* Ultima alteracao.: 29/10/2023
+* Nome.............: Filosofos
+* Funcao...........: Define os moldes dos filosofos, definindo as 
+										 acoes que serao realizadas por eles
+*************************************************************** */
+import java.util.concurrent.Semaphore;
+import javafx.application.Platform;
+import javafx.scene.image.ImageView;
+
+public class Filosofos extends Thread {
+
+  //Variavel que armazena o numero de filosofos
+  private static final int N = 5;
+
+  //Variaveis que definem o estado do filosofo
+  private static final int pensando = 0;
+  private static final int fome = 1;
+  private static final int comendo = 2;
+
+  //Variavel que identifica o filosofo
+  private int id;
+
+  //Flag para controlar o pause e retomar do processo
+  private boolean pause = false;
+
+  //Array que armazena as ImageView dos garfos da mesa
+  private ImageView[] garfos;
+
+  //ImageView do estado Pensado do filosofo
+  private ImageView filosofoPensando;
+  //ImageView do estado Normal do filosofo
+  private ImageView filosofoNormal;
+  //ImageView do estado Comendo do filosofo
+  private ImageView filosofoComendo;
+
+  //Definicao da velocidade inicial para as acoes de comer e pensar dos filosofos
+  private double tempoComer = 1;
+  private double tempoPensar = 1;
+
+  //Array que armazena o estado dos filosofos
+  private static int[] estado = new int[N];
+
+  //Semaforos para garantir o acesso unico ao recurso compartilhado
+  private static Semaphore mutex = new Semaphore(1);
+  private static Semaphore mutex2 = new Semaphore(1);
+  private static Semaphore mutex3 = new Semaphore(1);
+  private static Semaphore mutex4 = new Semaphore(1);
+  private static Semaphore mutex5 = new Semaphore(1);
+  private static Semaphore mutex6 = new Semaphore(1);
+  private static Semaphore[] semaforos = new Semaphore[N];
+
+  /* ***************************************************************
+   * Metodo: Filosofos
+   * Funcao: Construtor do objeto Filosofo.
+   * Parametros: Id do filosofo, array dos garfos, estados normal,
+   * 						 pensando e comendo do filosofo.
+   * Retorno: Sem retorno.
+   *************************************************************** */
+  public Filosofos(
+    int id,
+    ImageView[] garfo,
+    ImageView filosofoNormal,
+    ImageView filosofoPensando,
+    ImageView filosofoComendo
+  ) {
+    this.id = id;
+    semaforos[this.id] = new Semaphore(0);
+    this.garfos = garfo;
+    this.filosofoNormal = filosofoNormal;
+    this.filosofoPensando = filosofoPensando;
+    this.filosofoComendo = filosofoComendo;
+  }
+
+  /* ***************************************************************
+   * Metodo: run
+   * Funcao: Executa o processo do filosofo.
+   * Parametros: Sem parametros.
+   * Retorno: Sem retorno.
+   *************************************************************** */
+  public void run() {
+    while (!Thread.interrupted()) {
+      try {
+        pensar();
+        pegarGarfos();
+        comer();
+        largarGarfos();
+      } catch (InterruptedException e) {}
+    }
+  }
+
+  /* ***************************************************************
+   * Metodo: pensar
+   * Funcao: Realiza a etapa de pensamento do filosofo, alem de alterar
+   * 				 a velocidade com que pensa.
+   * Parametros: Sem parametros.
+   * Retorno: Sem retorno.
+   *************************************************************** */
+  public void pensar() throws InterruptedException {
+    sleep(((long) (100 / tempoPensar)));
+    pausando();
+  }
+
+  /* ***************************************************************
+   * Metodo: pensar
+   * Funcao: Realiza a etapa de alimentacao do filosofo, alem de
+   * 				 alterar a velocidade com que come
+   * Parametros: Sem parametros.
+   * Retorno: Sem retorno.
+   *************************************************************** */
+  public void comer() throws InterruptedException {
+    sleep(((long) (100 / tempoComer)));
+    pausando();
+  }
+
+  /* ***************************************************************
+   * Metodo: pegarGarfos
+   * Funcao: Realiza a manipulacao dos garfos na mesa, altera o estado
+   * 				 do filosofo e suas ImageViews para que a etapa de pensar
+   * 				 possa ser realizada.
+   * Parametros: Sem parametros.
+   * Retorno: Sem retorno.
+   *************************************************************** */
+  public void pegarGarfos() { // i=no do filosofo
+    try {
+      pausando();
+      mutex.acquire(); // Entra na RC
+      estado[id] = fome; // Lembra que o filosofo i quer comer
+      testarGarfo(id); // tenta adquirir 2 garfos
+      mutex.release(); // sai da rc
+      semaforos[id].acquire(); // bloqueia se não pegar 2 garfos
+      sleep((1000));
+      moveGarfos();
+
+      Platform.runLater(() -> {
+        filosofoNormal.setVisible(false);
+        filosofoPensando.setVisible(false);
+        filosofoComendo.setVisible(true);
+      });
+    } catch (InterruptedException e) {}
+  } // fim do metodo pegar garfos
+
+  /* ***************************************************************
+   * Metodo: largarGarfos
+   * Funcao: Realiza a manipulacao dos garfos na mesa, altera o estado
+   * 				 do filosofo e suas ImageViews para que a etapa de comer
+   * 				 possa ser realizada.
+   * Parametros: Sem parametros.
+   * Retorno: Sem retorno.
+   *************************************************************** */
+  public void largarGarfos() { // i=no do filosofo
+    try {
+      pausando();
+      mutex.acquire(); // Entra na RC
+      estado[id] = pensando; // filosofo libera os 2 garfos
+      testarGarfo(vizinhoEsquerda(id)); // olha se o vizinho ESQUERDA pode comer agora
+      testarGarfo(vizinhoDireita(id)); // olha se o vizinho DIREITA pode comer agora
+      mutex.release(); // sai da RC
+      sleep(1000);
+      voltaGarfos();
+      Platform.runLater(() -> {
+        filosofoNormal.setVisible(false);
+        filosofoPensando.setVisible(true);
+        filosofoComendo.setVisible(false);
+      });
+    } catch (InterruptedException e) {}
+  } // fim do metodo largar garfos
+
+  /* ***************************************************************
+   * Metodo: vizinhoEsquerda
+   * Funcao: Verifica quem e o vizinho a esquerda do filosofo.
+   * Parametros: id do filosofo.
+   * Retorno: O id do vizinho a esquerda do filosofo atual.
+   *************************************************************** */
+  public int vizinhoEsquerda(int i) {
+    return (i + N - 1) % 5;
+  }
+
+  /* ***************************************************************
+   * Metodo: vizinhoDireita
+   * Funcao: Verifica quem e o vizinho a direita do filosofo.
+   * Parametros: id do filosofo.
+   * Retorno: O id do vizinho a direita do filosofo atual.
+   *************************************************************** */
+  public int vizinhoDireita(int i) {
+    return (i + 1 + N) % 5;
+  }
+
+  /* ***************************************************************
+   * Metodo: testarGarfo
+   * Funcao: testa se e possivel adquirir os 2 garfos (direita e esquerda),
+   * 				 se nao for possivel ele bloqueia o estado e espera ate que seja
+   * 				 possivel.
+   * Parametros: id do filosofo.
+   * Retorno: Sem retorno.
+   *************************************************************** */
+  public void testarGarfo(int i) throws InterruptedException {
+    pausando();
+    if (
+      estado[i] == fome &&
+      estado[vizinhoEsquerda(i)] != comendo &&
+      estado[vizinhoDireita(i)] != comendo
+    ) {
+      estado[i] = comendo;
+      semaforos[i].release();
+    }
+  }
+
+  /* ***************************************************************
+   * Metodo: pausando
+   * Funcao: Pausa a thread.
+   * Parametros: Sem parametro.
+   * Retorno: Sem retorno.
+   *************************************************************** */
+  private void pausando() throws InterruptedException {
+    while (pause) {
+      sleep(1);
+    }
+  }
+
+  /* ***************************************************************
+   * Metodo: pausar
+   * Funcao: Altera o estado da flag utilizada para pausar/retomar a Thread.
+   * 				 Parando a thread e todo o seu ciclo.
+   * Parametros: Sem parametro.
+   * Retorno: Sem retorno.
+   *************************************************************** */
+  public void pausar() {
+    pause = true;
+  }
+
+  /* ***************************************************************
+   * Metodo: retomar
+   * Funcao: Altera o estado da flag utilizada para pausar/retomar a Thread.
+   * 				 fazendo com que a Thread volte para seu fluxo normal.
+   * Parametros: Sem parametro.
+   * Retorno: Sem retorno.
+   *************************************************************** */
+  public void retomar() {
+    pause = false;
+  }
+
+  /* ***************************************************************
+   * Metodo: alterarVelocidadeComer
+   * Funcao: Altera a velocidade com que o filosofo passa pelo processo
+   * 				 comer.
+   * Parametros: Valor double.
+   * Retorno: Sem retorno.
+   *************************************************************** */
+  public void alterarVelocidadeComer(double velocidade) {
+    this.tempoComer = velocidade;
+  }
+
+  /* ***************************************************************
+   * Metodo: alterarVelocidadePensar
+   * Funcao: Altera a velocidade com que o filosofo passa pelo processo
+   * 				 pensar.
+   * Parametros: Valor double.
+   * Retorno: Sem retorno.
+   *************************************************************** */
+  public void alterarVelocidadePensar(double velocidade) {
+    this.tempoPensar = velocidade;
+  }
+
+  public void moveGarfos() throws InterruptedException {
+    switch (id) {
+      case 0:
+        mutex2.acquire();
+        mutex6.acquire();
+        garfos[0].setX(20);
+        garfos[0].setY(30);
+        garfos[1].setX(-30);
+        garfos[1].setY(30);
+        break;
+      case 1:
+        mutex2.acquire();
+        mutex3.acquire();
+        garfos[1].setX(39);
+        garfos[1].setY(-16);
+        garfos[2].setX(13);
+        garfos[2].setY(33);
+        break;
+      case 2:
+        mutex3.acquire();
+        mutex4.acquire();
+        garfos[2].setX(-5);
+        garfos[2].setY(-39);
+        garfos[3].setX(33);
+        garfos[3].setY(9);
+        break;
+      case 3:
+        mutex4.acquire();
+        mutex5.acquire();
+        garfos[3].setX(-37);
+        garfos[3].setY(-16);
+        garfos[4].setX(24);
+        garfos[4].setY(-40);
+        break;
+      case 4:
+        mutex5.acquire();
+        mutex6.acquire();
+        garfos[4].setX(-21);
+        garfos[4].setY(43);
+        garfos[0].setX(-40);
+        garfos[0].setY(-34);
+        break;
+    }
+  }
+
+  public void voltaGarfos() {
+    switch (id) {
+      case 0:
+        garfos[0].setX(0);
+        garfos[0].setY(0);
+        garfos[1].setX(0);
+        garfos[1].setY(0);
+        mutex2.release();
+        mutex6.release();
+        break;
+      case 1:
+        garfos[1].setX(0);
+        garfos[1].setY(0);
+        garfos[2].setX(0);
+        garfos[2].setY(0);
+        mutex2.release();
+        mutex3.release();
+        break;
+      case 2:
+        garfos[2].setX(0);
+        garfos[2].setY(0);
+        garfos[3].setX(0);
+        garfos[3].setY(0);
+        mutex3.release();
+        mutex4.release();
+        break;
+      case 3:
+        garfos[3].setX(0);
+        garfos[3].setY(0);
+        garfos[4].setX(0);
+        garfos[4].setY(0);
+        mutex4.release();
+        mutex5.release();
+        break;
+      case 4:
+        garfos[4].setX(0);
+        garfos[4].setY(0);
+        garfos[0].setX(0);
+        garfos[0].setY(0);
+        mutex5.release();
+        mutex6.release();
+        break;
+    }
+  }
+}
---
+++ Jantar_Dos_Vampiros/controle.java
@@ -0,0 +1,318 @@
+/* ***************************************************************
+* Autor............: Cristhian Kauan Moreno Silveira
+* Matricula........: 202210185
+* Inicio...........: 23/10/2023
+* Ultima alteracao.: 29/10/2023
+* Nome.............: controle
+* Funcao...........: Manipula os objetos da interface e as classes 
+										 modelo.
+*************************************************************** */
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Slider;
+import javafx.scene.image.ImageView;
+
+public class controle {
+
+  //Array que armazena os filosofos
+  private Filosofos filosofos[] = new Filosofos[5];
+  //Array que armazena os ImageView dos garfos.
+  private ImageView[] forks = new ImageView[5];
+  //Flag para verificar se as threads dos filosofos já foi inicializada.
+  private int verificarInicialização = 0;
+
+  //Elementos FXML dos garfos da mesa
+  @FXML
+  private ImageView fork0, fork1, fork2, fork3, fork4;
+
+  //Elementos FXML dos filosofos, nessa aplicacao vampiros.
+  @FXML
+  private ImageView filosofoNormal1, filosofoNormal2, filosofoNormal3, filosofoNormal4, filosofoNormal5;
+
+  @FXML
+  private ImageView filosofoPensando1, filosofoPensando2, filosofoPensando3, filosofoPensando4, filosofoPensando5;
+
+  @FXML
+  private ImageView filosofoComendo1, filosofoComendo2, filosofoComendo3, filosofoComendo4, filosofoComendo5;
+
+  @FXML
+  private Slider sliderComer1, sliderComer2, sliderComer3, sliderComer4, sliderComer5;
+
+  @FXML
+  private Slider sliderPensar1, sliderPensar2, sliderPensar3, sliderPensar4, sliderPensar5;
+
+  /* ***************************************************************
+   * Metodo: resetAll
+   * Funcao: O objetivo era retornar a aplicacao ao seu estado
+   * 				inicial, mas nao consegui desenvolver de forma funcional,
+   * 				por isso o metodo apenas pausa a aplciacao.
+   * Parametros: Sem parametros
+   * Retorno: Sem retorno.
+   *************************************************************** */
+  @FXML
+  public void resetAll(ActionEvent event) throws InterruptedException {
+    for (int i = 0; i < 5; i++) {
+      filosofos[i].pausar();
+    }
+  }
+
+  /* ***************************************************************
+   * Metodo: start
+   * Funcao: Inicializa as Threads dos filosofos e despausa quando
+   * 				as Threads sao pausadas.
+   * Parametros: Sem parametros
+   * Retorno: Sem retorno.
+   *************************************************************** */
+  @FXML
+  public void start(ActionEvent event) throws InterruptedException {
+    if (verificarInicialização == 0) {
+      for (int i = 0; i < 5; i++) {
+        filosofos[i].start();
+      }
+      verificarInicialização = 1;
+    } else {
+      for (int i = 0; i < 5; i++) {
+        filosofos[i].retomar();
+      }
+    }
+  }
+
+  /* ***************************************************************
+   * Metodo: pauseFilosofo1
+   * Funcao: Pausa a execucao dos metodos da Thread filosofos[0]
+   * Parametros: ActionEvent event
+   * Retorno: Sem retorno.
+   *************************************************************** */
+  @FXML
+  public void pauseFilosofo1(ActionEvent event) {
+    filosofos[0].pausar();
+  }
+
+  /* ***************************************************************
+   * Metodo: pauseFilosofo2
+   * Funcao: Pausa a execucao dos metodos da Thread filosofos[1]
+   * Parametros: ActionEvent event
+   * Retorno: Sem retorno.
+   *************************************************************** */
+  @FXML
+  public void pauseFilosofo2(ActionEvent event) {
+    filosofos[1].pausar();
+  }
+
+  /* ***************************************************************
+   * Metodo: pauseFilosofo3
+   * Funcao: Pausa a execucao dos metodos da Thread filosofos[2]
+   * Parametros: ActionEvent event
+   * Retorno: Sem retorno.
+   *************************************************************** */
+  @FXML
+  public void pauseFilosofo3(ActionEvent event) {
+    filosofos[2].pausar();
+  }
+
+  /* ***************************************************************
+   * Metodo: pauseFilosofo4
+   * Funcao: Pausa a execucao dos metodos da Thread filosofos[3]
+   * Parametros: ActionEvent event
+   * Retorno: Sem retorno.
+   *************************************************************** */
+  @FXML
+  public void pauseFilosofo4(ActionEvent event) {
+    filosofos[3].pausar();
+  }
+
+  /* ***************************************************************
+   * Metodo: pauseFilosofo5
+   * Funcao: Pausa a execucao dos metodos da Thread filosofos[4]
+   * Parametros: ActionEvent event
+   * Retorno: Sem retorno.
+   *************************************************************** */
+  @FXML
+  public void pauseFilosofo5(ActionEvent event) {
+    filosofos[4].pausar();
+  }
+
+  /* ***************************************************************
+   * Metodo: retomarFilosofo1
+   * Funcao: Despausa a execucao dos metodos da Thread filosofos[0]
+   * Parametros: ActionEvent event
+   * Retorno: Sem retorno.
+   *************************************************************** */
+  @FXML
+  public void retomarFilosofo1(ActionEvent event) {
+    filosofos[0].retomar();
+  }
+
+  /* ***************************************************************
+   * Metodo: retomarFilosofo2
+   * Funcao: Despausa a execucao dos metodos da Thread filosofos[1]
+   * Parametros: ActionEvent event
+   * Retorno: Sem retorno.
+   *************************************************************** */
+  @FXML
+  public void retomarFilosofo2(ActionEvent event) {
+    filosofos[1].retomar();
+  }
+
+  /* ***************************************************************
+   * Metodo: retomarFilosofo3
+   * Funcao: Despausa a execucao dos metodos da Thread filosofos[2]
+   * Parametros: ActionEvent event
+   * Retorno: Sem retorno.
+   *************************************************************** */
+  @FXML
+  public void retomarFilosofo3(ActionEvent event) {
+    filosofos[2].retomar();
+  }
+
+  /* ***************************************************************
+   * Metodo: retomarFilosofo4
+   * Funcao: Despausa a execucao dos metodos da Thread filosofos[3]
+   * Parametros: ActionEvent event
+   * Retorno: Sem retorno.
+   *************************************************************** */
+  @FXML
+  public void retomarFilosofo4(ActionEvent event) {
+    filosofos[3].retomar();
+  }
+
+  /* ***************************************************************
+   * Metodo: retomarFilosofo5
+   * Funcao: Despausa a execucao dos metodos da Thread filosofos[4]
+   * Parametros: ActionEvent event
+   * Retorno: Sem retorno.
+   *************************************************************** */
+  @FXML
+  public void retomarFilosofo5(ActionEvent event) {
+    filosofos[4].retomar();
+  }
+
+  public void initialize() {
+    //Armazenando os ImageView dos garfos no array forks[5]
+    forks[0] = fork0;
+    forks[1] = fork1;
+    forks[2] = fork2;
+    forks[3] = fork3;
+    forks[4] = fork4;
+
+    //Instancia dos filosofos que sao armazenados no array filosofos[5]
+    filosofos[0] =
+      new Filosofos(
+        0,
+        forks,
+        filosofoNormal1,
+        filosofoPensando1,
+        filosofoComendo1
+      );
+    filosofos[1] =
+      new Filosofos(
+        1,
+        forks,
+        filosofoNormal2,
+        filosofoPensando2,
+        filosofoComendo2
+      );
+    filosofos[2] =
+      new Filosofos(
+        2,
+        forks,
+        filosofoNormal3,
+        filosofoPensando3,
+        filosofoComendo3
+      );
+    filosofos[3] =
+      new Filosofos(
+        3,
+        forks,
+        filosofoNormal4,
+        filosofoPensando4,
+        filosofoComendo4
+      );
+    filosofos[4] =
+      new Filosofos(
+        4,
+        forks,
+        filosofoNormal5,
+        filosofoPensando5,
+        filosofoComendo5
+      );
+
+    //Configuracao para que os sliders que definem a velocidade com que os filosofos comem tenham um valor minimo=1 e valor maximo=1000
+    sliderComer1.setMax(1000);
+    sliderComer1.setMin(1);
+    sliderComer2.setMax(1000);
+    sliderComer2.setMin(1);
+    sliderComer3.setMax(1000);
+    sliderComer3.setMin(1);
+    sliderComer4.setMax(1000);
+    sliderComer4.setMin(1);
+    sliderComer5.setMax(1000);
+    sliderComer5.setMin(1);
+
+    //Configuracao para os sliders que definem a velocidade com que os filosofos pensam tenham um valor minimo=1 e valor maximo=1000
+    sliderPensar1.setMax(1000);
+    sliderPensar1.setMin(1);
+    sliderPensar2.setMax(1000);
+    sliderPensar2.setMin(1);
+    sliderPensar3.setMax(1000);
+    sliderPensar3.setMin(1);
+    sliderPensar4.setMax(1000);
+    sliderPensar4.setMin(1);
+    sliderPensar5.setMax(1000);
+    sliderPensar5.setMin(1);
+
+    //Configurando para que sempre que houver uma alteracao nos slidersComer eles chamem o metodo alterarVelocidadeComer para o filosofo especifico
+    sliderComer1
+      .valueProperty()
+      .addListener((observable, oldValue, newValue) -> {
+        filosofos[0].alterarVelocidadeComer(sliderComer1.getValue());
+      });
+    sliderComer2
+      .valueProperty()
+      .addListener((observable, oldValue, newValue) -> {
+        filosofos[1].alterarVelocidadeComer(sliderComer2.getValue());
+      });
+    sliderComer3
+      .valueProperty()
+      .addListener((observable, oldValue, newValue) -> {
+        filosofos[2].alterarVelocidadeComer(sliderComer3.getValue());
+      });
+    sliderComer4
+      .valueProperty()
+      .addListener((observable, oldValue, newValue) -> {
+        filosofos[3].alterarVelocidadeComer(sliderComer4.getValue());
+      });
+    sliderComer5
+      .valueProperty()
+      .addListener((observable, oldValue, newValue) -> {
+        filosofos[4].alterarVelocidadeComer(sliderComer5.getValue());
+      });
+
+    //Configurando para que sempre que houver uma alteracao nos slidersPensar eles chamem o metodo alterarVelocidadePensar para o filosofo especifico
+    sliderPensar1
+      .valueProperty()
+      .addListener((observable, oldValue, newValue) -> {
+        filosofos[0].alterarVelocidadePensar(sliderPensar1.getValue());
+      });
+    sliderPensar2
+      .valueProperty()
+      .addListener((observable, oldValue, newValue) -> {
+        filosofos[1].alterarVelocidadePensar(sliderPensar2.getValue());
+      });
+    sliderPensar3
+      .valueProperty()
+      .addListener((observable, oldValue, newValue) -> {
+        filosofos[2].alterarVelocidadePensar(sliderPensar3.getValue());
+      });
+    sliderPensar4
+      .valueProperty()
+      .addListener((observable, oldValue, newValue) -> {
+        filosofos[3].alterarVelocidadePensar(sliderPensar4.getValue());
+      });
+    sliderPensar5
+      .valueProperty()
+      .addListener((observable, oldValue, newValue) -> {
+        filosofos[4].alterarVelocidadePensar(sliderPensar5.getValue());
+      });
+  }
+}
